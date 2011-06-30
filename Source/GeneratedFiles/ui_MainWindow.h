14fc46cc967338323ed61659f7b4625671502f0d
Rosalia Schneider <rosaliaschneider@gmail.com>
2011-06-30 19:22:31 -0300

    Removi alguns arquivos desnecessarios.

--- Source/GeneratedFiles/ui_MainWindow.h
+++ Source/GeneratedFiles/ui_MainWindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'MainWindow.ui'
 **
-** Created: Thu Jun 30 18:32:52 2011
+** Created: Thu Jun 30 19:11:11 2011
 **      by: Qt User Interface Compiler version 4.7.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
